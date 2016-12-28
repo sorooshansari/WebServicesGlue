1dcd3512c928f6a630258a60640cf09f4d113956
sorosh.ansari <sorosh.ansari@gmial.com>
2016-12-28 15:39:22 +0330

    test glue created

---
+++ testApp/testApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using testApp.accountingwebservice;
+
+namespace testApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountingWebServiceSoapClient client = new AccountingWebServiceSoapClient();
+            string res = client.GetNewMembersCodeByType(10, MemberType.Permanent, "SNMFAccounting", "SNMFAccounting");
+            Console.WriteLine(res);
+        }
+    }
+}
